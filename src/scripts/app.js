92d2c8991b40b7b0476e28e55313c6ecf1f91d95
Rodrigo Ramírez <dash1921@hotmail.com>
2016-06-06 15:48:20 -0500

    Añadimos navbar responsivo

--- src/scripts/app.js
+++
@@ -1,9 +0,0 @@
-$().ready(function () {
-	
-	var $img1 = $('.images').find('li');
-	var $izq = $('.flecha');
-
-	$izq.click(function() {
-		$img1.toggleClass('hidden');
-	});
-}());
---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+$().ready(function() {
+	var loadCSS     = require('./lib/loadCSS.js'),
+		onClickMenu = require('./lib/onClickMenu.js');
+	var $btnMenu = $('#btnMenu');
+	var $navbarMenu = $('#navbarMenu');
+    
+	$btnMenu.on('click', function () {
+    	$navbarMenu.toggleClass('header-menu-list--show');
+    });
+
+   	loadCSS('https://fonts.googleapis.com/css?family=Open+Sans');
+	loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css');
+
+	var $img1 = $('.images').find('li');
+	var $izq = $('.flecha');
+
+	$izq.click(function() {
+		$img1.toggleClass('hidden');
+	});
+});
